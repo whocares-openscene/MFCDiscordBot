4a4736acaf67bae3176c53260eb956dabc52b21f
whocares <whocares@open-scene.net>
2026-08-31 20:00:42 -0400

    Update webstuff.js

--- webstuff.js
+++ webstuff.js
@@ -4,20 +4,11 @@ import * as cheerio from 'cheerio';
 import axios from 'axios';
 import fs from 'fs';
 
-const options = {
-  method: 'GET',
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    'Accept': 'application/json',
-    'Cookie': 'pref_cli=w; baf=17686577664212444; color_scheme_dark_embed=true; _mfcs_perm=eyJfcmFpbHMiOnsibWVzc2FnZSI6IklqTmlNRGcxTVdabExUUTBaVGN0TkdFME15MWlNVGs1TFdWa1pXTTBObVEzTldReU9TST0iLCJleHAiOm51bGwsInB1ciI6ImNvb2tpZS5fbWZjc19wZXJtIn19--1bd919f83a1a09b5f541c86bf8bb090385270bec; window_size=1696x1043; _mfcs_session_v2=tY7uU63wSjqsUn25F2AQAJ8Y8Z%2B8yxKhGVEl2jfQ1r%2B82nPGqKyaobX%2FDdo7vsm%2BtE%2BJT%2BhbrSmTHQiMETOZKN7Epxx9ve1QF9glc9Sn8TY5IeHKTbnzaHD2CPnwUgZhounGurDXc14G3CvZbBNxqu3AsrrJZJR0dmxp58jd8fslzaWqu9iEMFbBpoRi%2BE%2BLlSEFuaDTiP9r37oevcL7CjUM%2FWJi3GanbUQj1FSm7oKUOY0fjwx0DEar0ue8A5U88MWsEDbu0QgejPWPISUKsL267c2HS4egdDHm3xKls2iY9HTU6UQYaRHeyoSzqA%3D%3D--oINqeqPD5BuHtsbR--972WzUVy3yzlhhNVJ0Le4w%3D%3D; g_state={"i_l":1,"i_ll":1787624231195,"i_b":"ajg3EDoYglc29HfU2IEkG125fGPRPejyl6v/ci4yjbA","i_e":{"enable_itp_optimization":24},"i_et":1787624231195}; ref=https%3A%2F%2Fshare.myfreecams.com%2F; browser_time_zone=America/New_York; ref_model_id=29845158; gw=true; calendar_filter_list_view=true; _gcl_au=1.1.1432150834.1787623582'
-  }
-};
-
 
 export async function gettopic(modelid) {
     var url = "https://api-edge.myfreecams.com/recommend?model_id=" + modelid +  "&version2=1&=";
     try {
-        const response = await fetch(url, options);
+        const response = await fetch(url);
         const body = await response.json();
         return body['result']['users'][modelid]['room_topic'];    
     } catch (error) {
@@ -30,7 +21,7 @@ export async function getmodelid(modelname) {
     ////console.log("function");
     var url = "https://api-edge.myfreecams.com/usernameLookup/" + modelname;
     try {
-        const response = await fetch(url, options);
+        const response = await fetch(url);
         const body = await response.json();
         if (body['result']['success'] === 0) {
             return "Lookup failed"
@@ -73,7 +64,7 @@ async function downloadImage(url, filepath) {
 export async function getCalendar(modelname) {
     var url = "https://share.myfreecams.com/" + modelname + "/calendar?list_view=true";
     try {
-        const response = await fetch(url, options);
+        const response = await fetch(url);
         const body = await response.text();
         if (response['status'] == 200) {
             const events = [];
@@ -110,7 +101,7 @@ export async function getstatus(modelid) {
     }
     //console.log(modelstatus);
     try {
-        const response = await fetch(url, options);
+        const response = await fetch(url);
         const body = await response.json();
         //console.log("Try");
         if (body['result']['success'] === 0) {
